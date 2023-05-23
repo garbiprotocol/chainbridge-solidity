94c37e194ae6525d0e57f4adbca593fabee62886
CoinHunter314159 <coinhunter314159@gmail.com>
2023-05-23 13:33:55 +0700

    update

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,8 +3,18 @@
  * SPDX-License-Identifier: LGPL-3.0-only
  */
 
-const Migrations = artifacts.require("Migrations");
+const Bridge = artifacts.require("Bridge");
+const ERC20Handler = artifacts.require("ERC20Handler");
+
+const domainID = 3;
+const initialRelayers = ["0x632414bbF1C1DE108Aec3Ff3B716ace89e582063", "0xeA3AA95e4E7E71e554d6c1E4717bb17992d6d75a", "0x8Be27cE5308fc7935B0513c2d71a623588FD78e2", "0xB098F3A963D6E608460f2aDA8f533da9178D72F4"];
+const initialRelayerThreshold = 2;
+const fee = 0;
+const expiry = 100;
+
+const bridgeAddress = "0xaab127696990703c922ad4ccdd5838799e6ba265";
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Bridge, domainID, initialRelayers, initialRelayerThreshold, fee, expiry);
+  //deployer.deploy(ERC20Handler, bridgeAddress);
 };
