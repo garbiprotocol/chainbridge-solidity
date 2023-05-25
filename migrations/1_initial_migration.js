56a330f5ba15d8a0fa783da659e42b03b4cff107
CoinHunter314159 <CoinHunter314159@gmail.com>
2023-05-25 14:06:03 +0700

    update

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -6,13 +6,13 @@
 const GarbiBridge = artifacts.require("GarbiBridge");
 const ERC20Handler = artifacts.require("ERC20Handler");
 
-const domainID = 42170;
+const domainID = 2;
 const initialRelayers = ["0x632414bbF1C1DE108Aec3Ff3B716ace89e582063", "0xeA3AA95e4E7E71e554d6c1E4717bb17992d6d75a", "0xc1Ad8A92e248a89Ee47b1705D9bfCD4798c97b4a"];
 const initialRelayerThreshold = 1;
 const fee = ""+3*10**18;
 const expiry = 100;
 
-const bridgeAddress = "0x0e99bb96880ed959324615d0e9cd05ec7f7eb004";
+const bridgeAddress = "0xa1aa773bd288a269d83db99e43d8bad6d8c9d2a0";
 const GRB = "0x90f72cc2bdfaf3ab5ac61a7f7b6759e25b844f1a";
 
 module.exports = function(deployer) {
